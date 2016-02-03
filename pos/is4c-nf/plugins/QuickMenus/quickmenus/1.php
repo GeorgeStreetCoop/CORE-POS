76747ee5da974ef6bab06ac833368ae1149f46b2
John Leary <john.leary@gmail.com>
2016-02-03 17:56:38 -0500

    Initial lane QuickMenus

--- pos/is4c-nf/plugins/QuickMenus/quickmenus/1.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/1.php
@@ -1,11 +1,1 @@
-<?php
-
-$my_menu = array(
-    "Charge Available Card" => "DATACAP",
-    "Charge Amount Due As Credit" => "DATACAPCCAUTO",
-    "Reset Terminal (Quick)" => "TERMRESET",
-    "Tender External CC" => "MANUALCC",
-    "Lookup Transaction" => "PCLOOKUP",
-    "Reboot Terminal (Slow)" => "TERMREBOOT",
-);
-
+GrabGo.php
--- pos/is4c-nf/plugins/QuickMenus/quickmenus/3.php
+++ pos/is4c-nf/plugins/QuickMenus/quickmenus/3.php
@@ -1,9 +1,1 @@
-<?php
-
-$my_menu = array(
-    "Bakery Other" => "99000",
-    "Bakery Bread" => "99001",
-    "Bakery Granola" => "99002",
-    "Bakery Sweets" => "99003"
-);
-
+Waters.php
